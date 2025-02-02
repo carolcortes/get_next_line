2f9a6cb0ae8a4999c1f046bf73137457985dbdba
Caroline De Oliveira Cortes <cade-oli@c1r4s4.42porto.com>
2025-02-02 16:19:33 +0000

    initial commit

---
+++ get_next_line.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/02 10:16:51 by cade-oli          #+#    #+#             */
+/*   Updated: 2025/02/02 16:19:00 by cade-oli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_free(char *s1, char *s2)
+{
+	char	*tmp;
+
+	tmp = ft_strjoin(s1, s2);
+	free(s1);
+	return (tmp);
+}
+
+char	*read_file(int fd, char *b)
+{
+	char	*block;
+	int		bytes;
+
+	if (!b)
+		b = ft_calloc(1, 1);
+	block = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	bytes = 1;
+	while (bytes > 0)
+	{
+		bytes = read(fd, block, BUFFER_SIZE);
+		if (bytes == -1)
+		{
+			free(block);
+			return (NULL);
+		}
+		block[bytes] = '\0';
+		b = ft_free(b, block);
+		if (ft_strchr(block, '\n'))
+			break ;
+	}
+	free(block);
+	return (b);
+}
+
+char	*get_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	line = ft_calloc(i + (buffer[i] == '\n') + 1, sizeof(char));
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] && buffer[i] == '\n')
+		line[i++] = '\n';
+	return (line);
+}
+
+char	*update_line(char *buffer)
+{
+	int		i;
+	int		j;
+	char	*line;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
+	i++;
+	j = 0;
+	while (buffer[i])
+		line[j++] = buffer[i++];
+	free(buffer);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	buffer = read_file(fd, buffer);
+	printf("buff: %s", buffer);
+	if (!buffer)
+		return (NULL);
+	line = get_line(buffer);
+	buffer = update_line(buffer);
+	return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/02 10:16:59 by cade-oli          #+#    #+#             */
+/*   Updated: 2025/02/02 15:59:28 by cade-oli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+#include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+
+#endif
