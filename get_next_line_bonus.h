b1cd4a9a55fa125d01dab222499970e9c438512e
carolcortes <caroline.ocortes@gmail.com>
2025-02-12 11:32:22 +0000

    fix: bonus header

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,20 +6,18 @@
 /*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 10:16:59 by cade-oli          #+#    #+#             */
-/*   Updated: 2025/02/11 22:15:37 by cade-oli         ###   ########.fr       */
+/*   Updated: 2025/02/12 11:30:52 by cade-oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
-# include <stdio.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 42
 # endif
 
 char	*get_next_line(int fd);
